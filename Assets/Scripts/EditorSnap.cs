bc02307d8c8f7a5c5d1762e16ff09535f80e8d2d
José Manuel <josemac95@gmail.com>
2019-09-23 14:49:00 +0200

    123. ExecuteInEditMode attribute

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+	// Posición ajustada
+	Vector3 snapPos;
+
+	// Tamaño de la cuadrícula de 1 a 20
+	[Range(1f, 20f)]
+	[SerializeField] float gridSize = 10f;
+
+	void Update()
+	{
+		snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+		snapPos.y = 0f;
+		snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+		transform.position = snapPos;
+	}
+}
