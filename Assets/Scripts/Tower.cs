3f942f9783cada70099266bd3a1ea9108c2bb40e
José Manuel <josemac95@gmail.com>
2019-09-28 12:30:24 +0200

    139. Tower look at enemy

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+	// La parte de la torre que se mueve
+	[SerializeField] Transform objectToPan = null;
+	// Enemigo al que se apunta
+	[SerializeField] Transform targetEnemy = null;
+
+	void Update()
+	{
+		// Apunta al enemigo
+		objectToPan.LookAt(targetEnemy);
+	}
+}
