51aba4bd9d5ec1b9922cf013c70399d720e4c9d4
José Manuel <josemac95@gmail.com>
2019-09-30 16:28:18 +0200

    144. Check for distance

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	// Vida (en número de impactos)
+	[SerializeField] int hits = 10;
+
+	void Start()
+	{
+		// Añade el componente via script
+		// Colisiones con partículas
+		AddNonTriggerBoxCollider();
+	}
+
+	// Añade el collider (No trigger)
+	private void AddNonTriggerBoxCollider()
+	{
+		Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+		// Por defecto es así, pero es mejor asegurarse
+		// Porque es importante para el funcionamiento
+		boxCollider.isTrigger = false;
+	}
+
+	// Si choca una partícula con el objeto
+	void OnParticleCollision(GameObject other)
+	{
+		// Para evitar doble muerte por colisión simultánea
+		if (hits > 0)
+		{
+			ProcessHit();
+			// Si está muerto
+			if (hits == 0)
+			{
+				KillEnemy();
+			}
+		}
+	}
+
+	// Procesa el impacto
+	private void ProcessHit()
+	{
+		// Queda menos vida
+		hits = hits - 1;
+	}
+
+	// Mata al enemigo
+	private void KillEnemy()
+	{
+		// Destruye el enemigo
+		Destroy(gameObject);
+	}
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -8,10 +8,55 @@ public class Tower : MonoBehaviour
 	[SerializeField] Transform objectToPan = null;
 	// Enemigo al que se apunta
 	[SerializeField] Transform targetEnemy = null;
+	// Partículas de disparo
+	[SerializeField] ParticleSystem bullets = null;
+	// Rango de la torre
+	[SerializeField] float attackRange = 10f;
 
 	void Update()
 	{
-		// Apunta al enemigo
-		objectToPan.LookAt(targetEnemy);
+		// Si hay enemigo
+		if (targetEnemy != null)
+		{
+			// Dispara al enemigo
+			FireAtEnemy();
+		}
+		else
+		{
+			// Desactiva las armas
+			ActivateGun(false);
+		}
+	}
+
+	// Dispara al enemigo dentro del rango
+	private void FireAtEnemy()
+	{
+		// Distancia
+		float distance = Vector3.Distance(targetEnemy.transform.position, transform.position);
+		// Está dentro de rango
+		bool isInRange = distance < attackRange;
+		// Sigue el movimiento
+		FollowEnemy(isInRange);
+		// Dispara
+		ActivateGun(isInRange);
+	}
+
+	// Sigue el movimiento
+	private void FollowEnemy(bool follow)
+	{
+		// Si lo puede seguir
+		if (follow)
+		{
+			// Apunta al enemigo
+			objectToPan.LookAt(targetEnemy);
+		}
+	}
+
+	// Dispara
+	private void ActivateGun(bool isActive)
+	{
+		// Activa o desactiva el módulo de emisión de partículas
+		var emissionModule = bullets.emission;
+		emissionModule.enabled = isActive;
 	}
 }
