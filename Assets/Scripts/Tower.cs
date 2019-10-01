9fa01b00f10484dc3398146f627b064aedbdc2ee
José Manuel <josemac95@gmail.com>
2019-10-01 13:45:46 +0200

    147. Target closest enemy

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -15,6 +15,8 @@ public class Tower : MonoBehaviour
 
 	void Update()
 	{
+		// Selecciona el enemigo más cercano
+		FindEnemy();
 		// Si hay enemigo
 		if (targetEnemy != null)
 		{
@@ -28,11 +30,49 @@ public class Tower : MonoBehaviour
 		}
 	}
 
+	// Encuentra el enemigo más cercano
+	private void FindEnemy()
+	{
+		// Colección de enemigos
+		Enemy[] enemies = FindObjectsOfType<Enemy>();
+		// Si no hay enemigos
+		if (enemies.Length == 0)
+		{
+			targetEnemy = null;
+		}
+		else
+		{
+			// Asume que el primero es el mejor
+			Transform closestEnemy = enemies[0].transform;
+			float minDistance = Vector3.Distance(closestEnemy.position, transform.position);
+			// Para cada uno
+			foreach (Enemy enemy in enemies)
+			{
+				(closestEnemy, minDistance) = GetClosestEnemy(minDistance, closestEnemy, enemy.transform);
+			}
+			// Actualiza el enemigo objetivo
+			targetEnemy = closestEnemy;
+		}
+	}
+
+	// Devuelve la tupla enemigo más cercano y su distancia
+	private (Transform, float) GetClosestEnemy(float minDistance, Transform closestEnemy, Transform newEnemy)
+	{
+		// Distancia actual
+		float distance = Vector3.Distance(newEnemy.position, transform.position);
+		if (distance < minDistance)
+		{
+			// Nuevo enemigo más cercano
+			return (newEnemy, distance);
+		}
+		return (closestEnemy, minDistance);
+	}
+
 	// Dispara al enemigo dentro del rango
 	private void FireAtEnemy()
 	{
 		// Distancia
-		float distance = Vector3.Distance(targetEnemy.transform.position, transform.position);
+		float distance = Vector3.Distance(targetEnemy.position, transform.position);
 		// Está dentro de rango
 		bool isInRange = distance < attackRange;
 		// Sigue el movimiento
