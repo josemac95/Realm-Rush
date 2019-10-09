22927844efb420c463567c703a193fc489ba84d7
José Manuel <josemac95@gmail.com>
2019-10-09 12:36:41 +0200

    157. Health for base

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+	// Vida de la base
+	[SerializeField] int health = 2;
+	// Puntos de vida que quita por enemigo
+	[SerializeField] int healthDecrease = 1;
+
+	// Si entra un enemigo
+	void OnTriggerEnter(Collider other)
+	{
+		// Para evitar doble muerte por colisión simultánea
+		if (health > 0)
+		{
+			ProcessHit();
+			// Si está muerto
+			if (health == 0)
+			{
+				GameOver();
+			}
+		}
+	}
+
+	// Procesa el impacto
+	private void ProcessHit()
+	{
+		// Queda menos vida
+		health = health - healthDecrease;
+	}
+
+	// Fin del juego
+	private void GameOver()
+	{
+		print("FIN");
+	}
+}
